bde115549b99f5b6ef6c32638229d2398e65ba50
Kyle Hall <kyle@bywatersolutions.com>
2022-12-06 09:41:54 -0500

    Fix config downloader

--- stale-tickets-notifier.py
+++ stale-tickets-notifier.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from urllib import request
+import urllib
 import os
 import rt
 import slack
@@ -31,11 +31,8 @@ bot = slack.WebClient(
 tracker = rt.Rt(rt_url + "/REST/1.0/", rt_username, rt_password)
 tracker.login()
 
-with open(r'config.yml') as file:
-    config = yaml.load(file, Loader=yaml.FullLoader)
-
-    y = urllib.request.urlopen(conf_url)
-    config = yaml.load(y, Loader=yaml.FullLoader)
+y = urllib.request.urlopen(conf_url)
+config = yaml.load(y, Loader=yaml.FullLoader)
 
 # Build dict of email => id
 users = client.users_list()
